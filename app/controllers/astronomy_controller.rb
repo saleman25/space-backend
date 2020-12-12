bbee6e4c4d59fb05a0ced8f0ae3818bf71e37cac
Samantha Aleman <samantha.aleman25@gmail.com>
2020-12-11 17:41:15 -0800

    added create to astronomy controller

--- app/controllers/astronomy_controller.rb
+++ app/controllers/astronomy_controller.rb
@@ -10,4 +10,19 @@ class AstronomyController < ApplicationController
         render json: astronomy
     end 
     
+    def create
+        astronomy = Astronomy.new(astronomy_params)
+        if astronomy.save
+            render json: astronomy
+        else
+            render json: { errors: "oops something went wrong"}
+        end 
+    end 
+
+    private 
+
+    def astronomy_params
+        params.require(:astronomy).permit(:date, :explanation, :url)
+    end 
+
 end 
